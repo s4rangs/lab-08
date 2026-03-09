02e9e2e7d4260e57237d639bf0f5f6d19d77150d
s4rangs <sarang2@ualberta.ca>
2026-03-09 12:02:08 -0600

    countCity succeed

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -16,6 +16,9 @@ public class CustomList {
     public void deleteCity(City city) {
         cities.remove(city);
     }
+    public int countCities() {
+        return cities.size();
+    }
 
     public boolean hasCity(City city) {
         return cities.contains(city);
